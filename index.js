8d8b789746479cd3c8e41e114c42041a4957d6c7
theus deka <me@theus.be>
2019-08-20 21:20:42 +0200

    Refactored with cryptojs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const hashcashToken = require('hashcash-token');
-const moment = require('moment');
-const DELIMITER = "|"
-/**
- *
- */
+import CryptoJS from 'crypto-js';
+
+
 export default function hashcash() {
-    function toJSONString( form ) {
+    const DELIMITER = "|";
+
+    function toJSONString( form )
+    {
         var obj = {};
         var elements = form.querySelectorAll( "input, select, textarea" );
         for( var i = 0; i < elements.length; ++i ) {
@@ -21,33 +21,50 @@ export default function hashcash() {
         return JSON.stringify( obj );
     }
 
+    function generateHashcash(form, hashcash_value)
+    {
+        const cash = form.getAttribute('data-hashcash');
+        const algo = form.getAttribute('data-hashalgo') || 'sha256';
+        const noun = form.getAttribute('data-hashnoun') || 4;
 
+        if(hashcash_value) {
+            return hashcash_value;
+        }
+        let random, value, hash, counter = 0;
+        const regex = new RegExp("^0{"+noun+"}");
+        const t0 = performance.now();
 
+        do {
+            random = Math.random().toString(36).substring(2, 15);
+            value = [noun, cash, random].join(DELIMITER);
+            hash = CryptoJS[algo.toUpperCase()](value).toString();
+            ++counter;
+        }
+        while (!hash.match(regex));
+
+        const t1 = performance.now();
+        console.log(`Hashcash generated with ${counter} iterations in ${t1-t0} ms`);
+
+        return value;
+    }
 
     console.log('Hashcash is initializing forms');
     const forms = document.querySelectorAll('form[data-hashcash]');
     for (const form of forms) {
-        // console.log(form);
-        const target = form.getAttribute('data-hashcash');
+
+        let hashcash_value = null;
+
         form.addEventListener('submit', function (e) {
             e.preventDefault();
-            // e.target.style.display = "none";
+
+            hashcash_value = generateHashcash(form, hashcash_value);
 
             const req = new XMLHttpRequest();
-
-            const data = moment().format('YYMMDD')+DELIMITER+target;
-
-            var token = hashcashToken.generate({
-                difficulty: 16384,
-                data: data
-            });
-
-
 
             req.open('POST', e.target.getAttribute('action'), true);
             req.setRequestHeader('Content-type', 'application/json');
             req.overrideMimeType("application/json");
-            req.setRequestHeader('X-Hashcash', [token.difficulty, token.data, token.nonce].join('|'));
+            req.setRequestHeader('X-Hashcash', hashcash_value);
 
             req.addEventListener("progress", function(evt) {
                 console.log('progress');
@@ -60,11 +77,7 @@ export default function hashcash() {
                 alert('error');
             }, false);
 
-
             req.send(toJSONString(e.target));
-
-
-
 
         }, false);
     }
