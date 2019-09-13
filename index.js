7050da92fa99dc8ee20f4cf5fde7697036c6b8ed
Mathieu De Keyzer <mathieu@dekeyzer.net>
2019-09-13 18:16:06 +0200

    Delays to allow frontend to interact with the user on submit

--- index.js
+++ index.js
@@ -3,34 +3,19 @@ import CryptoJS from 'crypto-js';
 
 export default function hashcash(target, onSubmitCallback, onSuccessCallback, onErrorCallback, onProgressCallback, DELIMITER) {
 
-    function toJSONString( form )
+    function generateHashcash(form)
     {
-        var obj = {};
-        var elements = form.querySelectorAll( "input, select, textarea" );
-        for( var i = 0; i < elements.length; ++i ) {
-            var element = elements[i];
-            var name = element.name;
-            var value = element.value;
+        let value = form.getAttribute('data-hashcash-value') || false;
 
-            if( name ) {
-                obj[ name ] = value;
-            }
+        if(value) {
+            return value;
         }
 
-        return JSON.stringify( obj );
-    }
-
-    function generateHashcash(form, hashcash_value)
-    {
         const cash = form.getAttribute('data-hashcash');
         const algo = form.getAttribute('data-hashalgo') || 'sha256';
         const noun = form.getAttribute('data-hashnoun') || 4;
 
-        if(hashcash_value) {
-            return hashcash_value;
-        }
-
-        let random, value, hash, counter = 0;
+        let random, hash, counter = 0;
         const regex = new RegExp("^0{"+noun+"}");
         const t0 = performance.now();
 
@@ -44,8 +29,69 @@ export default function hashcash(target, onSubmitCallback, onSuccessCallback, on
 
         const t1 = performance.now();
         console.log(`Hashcash generated with ${counter} iterations in ${t1-t0} ms`);
+        form.setAttribute('data-hashcash-value', value);
 
         return value;
+    }
+
+    function treatSubmit(form, e)
+    {
+        const hashcash_value = generateHashcash(form);
+
+        const req = new XMLHttpRequest();
+
+        req.open('POST', e.target.getAttribute('action'), true);
+        req.setRequestHeader('X-Hashcash', hashcash_value);
+
+        req.addEventListener("progress", function(evt) {
+            if (onProgressCallback) {
+                onProgressCallback(e.target, evt);
+            }
+            else {
+                console.log("progress");
+            }
+        }, false);
+        req.addEventListener("load", function(evt) {
+            try {
+                const jsonResponse = JSON.parse(req.responseText);
+
+                if ( jsonResponse.ouuid && jsonResponse.success ) {
+                    if (onSuccessCallback) {
+                        onSuccessCallback(e.target, jsonResponse.ouuid);
+                    }
+                    else {
+                        console.log('Your submit id: '+jsonResponse.ouuid);
+                    }
+                }
+                else if ( jsonResponse.message ) {
+                    // noinspection ExceptionCaughtLocallyJS
+                    throw jsonResponse.message;
+                }
+                else {
+                    // noinspection ExceptionCaughtLocallyJS
+                    throw evt.toString();
+                }
+            } catch(e) {
+                if (onErrorCallback) {
+                    onErrorCallback(e.target, e.toString());
+                }
+                else {
+                    console.log(e.toString());
+                }
+            }
+
+        }, false);
+        req.addEventListener("error", function(evt) {
+            if (onErrorCallback) {
+                onErrorCallback(e.target, evt.toString());
+            }
+            else {
+                console.log(evt.toString());
+            }
+        }, false);
+
+        const formData = new FormData(e.target);
+        req.send(formData);
     }
 
     if (!DELIMITER) {
@@ -56,11 +102,8 @@ export default function hashcash(target, onSubmitCallback, onSuccessCallback, on
     const forms = target.querySelectorAll('form[data-hashcash]');
     for (const form of forms) {
 
-        let hashcash_value = null;
-
         form.addEventListener('submit', function (e) {
             e.preventDefault();
-
 
             if (onSubmitCallback) {
                 onSubmitCallback(e.target);
@@ -68,61 +111,8 @@ export default function hashcash(target, onSubmitCallback, onSuccessCallback, on
             else {
                 console.log("submit");
             }
-
-            hashcash_value = generateHashcash(form, hashcash_value);
-
-            const req = new XMLHttpRequest();
-
-            req.open('POST', e.target.getAttribute('action'), true);
-            req.setRequestHeader('X-Hashcash', hashcash_value);
-
-            req.addEventListener("progress", function(evt) {
-                if (onProgressCallback) {
-                    onProgressCallback(e.target, evt);
-                }
-                else {
-                    console.log("progress");
-                }
-            }, false);
-            req.addEventListener("load", function(evt) {
-                try {
-                    const jsonResponse = JSON.parse(req.responseText);
-
-                    if ( jsonResponse.ouuid && jsonResponse.success ) {
-                        if (onSuccessCallback) {
-                            onSuccessCallback(e.target, jsonResponse.ouuid);
-                        }
-                        else {
-                            console.log('Your submit id: '+jsonResponse.ouuid);
-                        }
-                    }
-                    else if ( jsonResponse.message ) {
-                        throw jsonResponse.message;
-                    }
-                    else {
-                        throw evt.toString();
-                    }
-                } catch(e) {
-                    if (onErrorCallback) {
-                        onErrorCallback(e.target, e.toString());
-                    }
-                    else {
-                        console.log(e.toString());
-                    }
-                }
-
-            }, false);
-            req.addEventListener("error", function(evt) {
-                if (onErrorCallback) {
-                    onErrorCallback(e.target, evt.toString());
-                }
-                else {
-                    console.log(evt.toString());
-                }
-            }, false);
-
-            const formData = new FormData(e.target);
-            req.send(formData);
+            //some delay to allow client to do something (like a waiting modal)
+            setTimeout(treatSubmit, 50, form, e);
 
         }, false);
     }
